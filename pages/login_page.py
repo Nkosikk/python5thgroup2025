dbc256b1784bf887cfcf3c30cbc6269033749d06
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-04-23 19:25:33 +0200

    Created home_page.py, login_page.py and landing_page.py classes

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support import wait
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class login_page:
+
+    email_input_id = "login-email"
+    password_input_id = "login-password"
+    login_button_id = "login-submit"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_username(self, username):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(self.driver.find_element("id", self.email_input_id)).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element("id", self.password_input_id).send_keys(password)
+
+    def click_login_button(self):
+        self.driver.find_element("id", self.login_button_id).click()
