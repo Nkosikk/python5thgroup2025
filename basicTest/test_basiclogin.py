af3496f60131e61f41c74247a8886013f50ad751
molobeam <molobeam@gmail.com>
2026-04-23 20:08:42 +0200

    basic test created

--- basicTest/test_basiclogin.py
+++ basicTest/test_basiclogin.py
@@ -14,7 +14,7 @@ class TestBasicLogin:
         time.sleep(20)
         self.driver.maximize_window()
         self.driver.find_element("xpath", "//div[@class='nav-user-section']").click()
-        self.driver.find_element("id", "login-email").send_keys("nkwanyana@gmail.com")
+        self.driver.find_element("id", "login-email").send_keys("admin@gmail.com")
         self.driver.find_element("id", "login-password").send_keys("@12345678")
         self.driver.find_element("id", "login-submit").click()
 
